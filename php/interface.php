89c7adc62c15dd47dee5038ba31bb0c23e645ef4
EzekielCerezo <21-30381@g.batstate-u.edu.ph>
2023-11-19 00:14:03 +0800

    ITO AY KUNG MAGBABAGO

---
+++ php/interface.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
+
+    <title>Document</title>
+</head>
+
+<body>
+    <section id="header">
+        <a href="#"><img src="aluminum.png" class="logo" alt=""></a>
+
+        <div>
+            <ul id="navbar">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#product">Product</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </section>
+</body>
+
+</html>
